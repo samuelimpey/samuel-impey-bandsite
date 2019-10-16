2e9690d7ba9c747a099256f6c7079d63f44a2e1e
Samuel Impey <samuelimpey@Sra.localdomain>
2019-10-16 16:27:20 -0700

    Shows table eddits in js

--- sprint-2/javaScript/showsTable.js
+++ sprint-2/javaScript/showsTable.js
@@ -7,33 +7,57 @@ var shows = [
     { date: "Wed Aug 11 2019", venue: "Pres Club", location: "San Francisco, CA" }
 ];
 
-function createTable(table, shows) {
+function createTableMobile(table, shows) {
     for (var show of shows) {
-        createTableHead(table, showKeys);
-        var row = table.insertRow();
+
         for (key in show) {
-            var cell = row.insertCell();
-            var text = document.createTextNode(show[key]);
+
+            let row = table.insertRow();
+            createTableHead(showKeys, row);
+            let cell = row.insertCell();
+            let text = document.createTextNode(show[key]);
             cell.appendChild(text);
+            if (key === "date") {
+                let element = document.querySelectorAll('td');
+                console.log(element);
+                for (i = 0; i < element.length; i++) {
+                    if (i % 4 === 0) {
+                        element[i].classList.add("bolded-date");
+                    }
+                }
+            }
+
+            if (count % 3 === 2) {
+                let row = table.insertRow();
+                let buttoncell = row.insertCell();
+                let btn = document.createElement("BUTTON");
+                btn.innerHTML = "BUY TICKETS";
+                buttoncell.appendChild(btn);
+            }
+            count++;
+
         }
     }
-}
-
-function createTableHead(table, shows) {
-    var tHead = table.createTHead();
-    var row = tHead.insertRow();
-    for (var show of shows) {
-        var th = document.createElement("th");
-        var text = document.createTextNode(show);
+    function createTableHead(shows, row) {
+        let variable = count % 3;
+        // let tHead = table.createTHead();
+        // let row = table.insertRow();
+        let th = document.createElement("th");
+        let text = document.createTextNode(shows[variable]);
         th.appendChild(text);
         row.appendChild(th);
     }
 }
 
-var table = document.querySelector("table");
+
+
+
+let count = 3;
+// let countTwo = 4;
+const table = document.querySelector("table");
 //console.log(table);
-var showKeys = Object.keys(shows[0]);
+const showKeys = Object.keys(shows[0]);
 
 // function invocation
-createTable(table, shows); // actually pass the students array
+createTableMobile(table, shows); // actually pass the students array
 //createTableHead(table, showKeys);
